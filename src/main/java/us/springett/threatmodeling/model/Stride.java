b5e2c5cabb00db45a2b0aafc908db37e8f5fc9d4
stevespringett <steve@springett.us>
2016-10-14 22:58:10 -0500

    Added javadoc stride descriptions

--- src/main/java/us/springett/threatmodeling/model/Stride.java
+++ src/main/java/us/springett/threatmodeling/model/Stride.java
@@ -19,6 +19,36 @@ package us.springett.threatmodeling.model;
  * The Stride threat classification system developed by Microsoft and later
  * implemented by many organizations.
  *
+ * SPOOFING:
+ * Spoofing is when a process or entity is something other than its claimed
+ * identity. Examples include substituting a process, a file, website or a
+ * network address.
+ *
+ * TAMPERING:
+ * Tampering is the act of altering the bits. Tampering with a process
+ * involves changing bits in the running process. Similarly, Tampering with
+ * a data flow involves changing bits on the wire or between two running
+ * processes.
+ *
+ * REPUDIATION:
+ * Repudiation threats involve an adversary denying that something happened.
+ *
+ * INFORMATION DISCLOSURE:
+ * Information disclosure happens when the information can be read by an
+ * unauthorized party.
+ *
+ * DENIAL OF SERVICE:
+ * Denial of Service happens when the process or a datastore is not able
+ * to service incoming requests or perform up to spec.
+ *
+ * ELEVATION OF PRIVILEGE:
+ * A user subject gains increased capability or privilege by taking
+ * advantage of an implementation bug.
+ *
+ * ABUSE:
+ * Abuse is when a legitimate user violates the terms of use for the
+ * system without violating a system security policy.
+ *
  * @since 1.0.0
  */
 public enum Stride implements ThreatClassification {
